929509dfa819fc6a61b480ed54304c1d660ace04
bdarey <bdarey@gmail.com>
2018-12-11 23:36:34 +0000

    cleaned up my project into appropriate compoenents

--- src/app/postlogin/postlogin.component.ts
+++ src/app/postlogin/postlogin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActionService } from '../action.service';
+import { Router } from '@angular/router';
+import { AppUserService } from '../app-user.service';
 
 @Component({
   selector: 'app-postlogin',
@@ -6,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./postlogin.component.css']
 })
 export class PostloginComponent implements OnInit {
+    popular_movies: any;
+    upcoming_movies: any;
+    // search_result: any;
+    movie: any;
+  
+  constructor(public _action : ActionService, public _appUser : AppUserService) { 
 
-  constructor() { }
 
+   // get upcoming movies
+    this._action.getUpcomingMovies().subscribe(data => {
+        this.upcoming_movies = data['results'];
+        console.log(this.upcoming_movies);
+    });
+    
+    // get popular movies
+    
+    this._action.getPopularMovies().subscribe(data => {
+      this.popular_movies = data['results'];
+      // console.log(this.popular_movies);
+    });
+    
+}
+    
+  
+      // get search results of movies
+  // searchMovies() {
+  //   this._action.searchMovie(this.movie).subscribe(data => {
+  //     this.search_result = data['results'];
+  //     console.log(this.search_result);
+  //   });
+  // }
+  
+
+  saveFavorites (save) {
+    console.log(save)
+    this._appUser.postFav(window.sessionStorage.userId, window.sessionStorage.token, {"title" : save} )
+    .subscribe (
+      (data:any) => {
+        console.log(data)
+      })
+  } 
+    
   ngOnInit() {
   }
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,57 +10,26 @@ import { AppUserService } from '../app-user.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-    popular_movies: any;
-    upcoming_movies: any;
     search_result: any;
-    movie: any;
   
    
     
-  constructor( public _action : ActionService, public _appUser : AppUserService) { 
-    // get upcoming movies
-    this._action.getUpcomingMovies().subscribe(data => {
-        this.upcoming_movies = data['results'];
-        console.log(this.upcoming_movies);
-    });
+  constructor( public _action : ActionService, public _appUser : AppUserService) {}
     
-    // get popular movies
     
-    this._action.getPopularMovies().subscribe(data => {
-      this.popular_movies = data['results'];
-      // console.log(this.popular_movies);
-    });
-    
-  }
-      // get search results of movies
+          // get search results of movies
   searchMovies() {
     this._action.searchMovie(this.movie).subscribe(data => {
       this.search_result = data['results'];
       console.log(this.search_result);
     });
   }
-  
-  
-  saveFavorites (save) {
-    console.log(save)
-    this._appUser.postFav(window.sessionStorage.userId, window.sessionStorage.token, {"title" : save} )
-    .subscribe (
-      (data:any) => {
-        console.log(data)
-      })
-      
-  
-      
-      
-  } 
-    
     
   
     
+  ngOnInit() {}
 
-    
-  ngOnInit() {
-  }
+
 
 
 }
